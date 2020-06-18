59b7b3b342e44347922a63e95c81acae5a3264c7
nuno1000 <nunomiguel_slb_12@hotmail.com>
2020-06-18 13:09:57 +0100

    add some files

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ const VERIFY_TOKEN = process.env.VERIFICATION_TOKEN;
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const dotenv = require('dotenv');
 const path = require('path');
-const restify = require('restify');
-const express = require('express'),
-const body_parser = require('body-parser'),
-const app = express().use(body_parser.json()); // creates express http server
+//const restify = require('restify');
+
+// Imports dependencies and set up http server
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json()); // creates express http server
 
 // Import required bot services.
 // See https://aka.ms/bot-services to learn more about the different parts of a bot.
@@ -27,9 +30,9 @@ const ENV_FILE = path.join(__dirname, '.env');
 dotenv.config({ path: ENV_FILE });
 
 // Create HTTP server
-const server = restify.createServer();
-app.listen(process.env.port || process.env.PORT || 3978 || 1337, () => {
-    console.log(`\n${ server.name } listening to ${ server.url }`);
+//const server = restify.createServer();
+app.listen(process.env.port || process.env.PORT || 1337, () => {
+    //console.log(`\n${ server.name } listening to ${ server.url }`);
     console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
     console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
 });
@@ -68,12 +71,38 @@ adapter.onTurnError = onTurnErrorHandler;
 const bot = new Bot();
 
 // Listen for incoming requests.
-app.post('/api/messages', (req, res) => {
+/*app.post('/api/messages', (req, res) => {
     adapter.processActivity(req, res, async (context) => {
         // Route to main dialog.
         await bot.run(context);
     });
-})
+});*/
+
+// Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {  
+ 
+    let body = req.body;
+  
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
+  
+      // Iterates over each entry - there may be multiple if batched
+      body.entry.forEach(function(entry) {
+  
+        // Gets the message. entry.messaging is an array, but 
+        // will only ever contain one message, so we get index 0
+        let webhook_event = entry.messaging[0];
+        console.log(webhook_event);
+      });
+  
+      // Returns a '200 OK' response to all requests
+      res.status(200).send('EVENT_RECEIVED');
+    } else {
+      // Returns a '404 Not Found' if event is not from a page subscription
+      res.sendStatus(404);
+    }
+  
+  });
 
 // Listen for Upgrade requests for Streaming.
 app.on('upgrade', (req, socket, head) => {
@@ -92,30 +121,30 @@ app.on('upgrade', (req, socket, head) => {
     });
 });
 
-// Accepts GET requests at the /webhook endpoint
+// Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
 
-    /** UPDATE YOUR VERIFY TOKEN **/
-    const VERIFY_TOKEN = process.env.VERIFICATION_TOKEN;
-  
-    // Parse params from the webhook verification request
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = "teste"
+      
+    // Parse the query params
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
     let challenge = req.query['hub.challenge'];
-  
-    // Check if a token and mode were sent
+      
+    // Checks if a token and mode is in the query string of the request
     if (mode && token) {
-  
-      // Check the mode and token sent are correct
+    
+      // Checks the mode and token sent is correct
       if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-  
-        // Respond with 200 OK and challenge token from the request
+        
+        // Responds with the challenge token from the request
         console.log('WEBHOOK_VERIFIED');
         res.status(200).send(challenge);
-  
+      
       } else {
         // Responds with '403 Forbidden' if verify tokens do not match
-        res.sendStatus(403);
+        res.sendStatus(403);      
       }
     }
   });
